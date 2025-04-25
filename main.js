c17d5508250a84f7bbf9dd198e9d790b1605bac4
Dhanushmathan <lovelymathan2394@gmail.com>
2025-04-25 16:37:31 +0530

    feat: route sturcture

--- main.js
+++ main.js
@@ -7,6 +7,21 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
 })
 
+
+// CRUD Functionality of movies
+
+// R - For READING movies
+app.get('/movies', (req, res) => {});
+
+// C - For CREATING movies
+app.post('/movies', (req, res) => {});
+
+// U - For UPDATING movies
+app.put('/movies/:id', (req, res) => {});
+
+// D - For DELETING movies
+app.delete('/movies/:id', (req, res) => {});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
