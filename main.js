16714f944c136c2e259cd6a2e9164307ce5fd3fb
Dhanushmathan <lovelymathan2394@gmail.com>
2025-04-26 14:51:23 +0530

    connect to mongodb with env file

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import router from './routes/moviesRoute.js';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = 3000;
 
+// dotenv
+dotenv.config();
+
+// Connect DataBase
+connectDB();
+
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
-})
+});
 
 // CRUD Functionality of movies
 app.use('/movies', router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
